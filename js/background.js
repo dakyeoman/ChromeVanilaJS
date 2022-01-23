9e987f29e06de4535a637acc1f41d5d8ab48d674
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2022-01-23 23:10:59 +0900

    done

--- js/background.js
+++ js/background.js
@@ -1,8 +1,11 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)]; 
 //Math.random = random number between 0&1 -> Math.floor (ex)4.33 -> 4
+const chosenImage = images[Math.floor(Math.random() * images.length)]; 
 
 const bgImage = document.createElement("img"); //create HTML object
 bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage) //add html to body *from JS!
+
+//add html to body *from JS! Use appendChild or prependChild
+document.body.appendChild(bgImage);
+
